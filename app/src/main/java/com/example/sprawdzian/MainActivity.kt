e2d88201416f210933d7f8013c36e1ba935a0bdd
MarceliGG <marcel.giemza@outlook.com>
2024-12-16 15:24:00 +0100

    sp

--- app/src/main/java/com/example/sprawdzian/MainActivity.kt
+++ app/src/main/java/com/example/sprawdzian/MainActivity.kt
@@ -1,47 +1,110 @@
 package com.example.sprawdzian
 
 import android.os.Bundle
+import android.widget.Toast
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Button
+import androidx.compose.material3.Divider
+import androidx.compose.material3.HorizontalDivider
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.focus.focusModifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import com.example.sprawdzian.ui.theme.SprawdzianTheme
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
         setContent {
-            SprawdzianTheme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
-                }
-            }
+            Screen()
         }
     }
+
+
 }
 
 @Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
-
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    SprawdzianTheme {
-        Greeting("Android")
+fun Screen() {
+    var context = LocalContext.current;
+    Column(
+        modifier = Modifier
+            .fillMaxSize()
+            .padding(16.dp)
+    ) {
+        Row(
+            modifier = Modifier
+                .padding(10.dp)
+                .height(60.dp)
+        ) {
+            Image(
+                painter = painterResource(R.drawable.user_icon), "User Icon",
+                modifier = Modifier
+                    .fillMaxHeight()
+            )
+            Column(
+                modifier = Modifier
+                    .fillMaxHeight()
+                    .padding(10.dp),
+                verticalArrangement = Arrangement.Center,
+            ) {
+                Text(
+                    "Marcel Giemza",
+                    style = TextStyle(fontWeight = FontWeight.Bold, fontSize = 16.sp)
+                )
+                Text("Git statistics", style = TextStyle(fontSize = 12.sp, color = Color.Gray))
+            }
+        }
+        HorizontalDivider()
+        Column (
+            modifier = Modifier
+                .padding(16.dp)
+        ){
+            Text(
+                "Recent Activities",
+                style = TextStyle(fontWeight = FontWeight.Bold, fontSize = 24.sp)
+            )
+            Column(
+                modifier = Modifier
+                    .fillMaxHeight(0.9f)
+            ) { }
+        }
+        HorizontalDivider()
+        Box(
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(16.dp)
+        ) {
+            Button(
+                onClick = {
+                    Toast.makeText(context, "Well done!", Toast.LENGTH_SHORT).show()
+                },
+                modifier = Modifier
+                    .fillMaxWidth()
+            ) {
+                Text("Display Message")
+            }
+        }
     }
 }
